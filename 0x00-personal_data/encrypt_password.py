eb1492de9232511e71a351cc5bb7d26edb2a58ca
Adeoba Emmanuel Tosin <dbtosine4real@gmail.com>
2024-01-29 01:59:26 +0000

    personal_data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,12 +5,12 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-        """Hashes a password using a random salt.
-            """
-        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    """Hashes a password using a random salt.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-        def is_valid(hashed_password: bytes, password: str) -> bool:
-                    """Checks is a hashed password was formed from the given password.
-                        """
-        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks is a hashed password was formed from the given password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
